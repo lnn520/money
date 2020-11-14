5959b89088c7793b23e1d6aefa187e8df0aca91a
lnn520 <335341543@qq.com>
2020-11-14 15:33:49 +0800

    添加404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>你当前路径不存在</div>
+    <dvi> <router-link to="/">返回</router-link>    </dvi>
+  </div>
+
+</template>
+
+<script>
+export default {
+name: "NotFound"
+}
+</script>
+
+<style scoped>
+
+</style>
