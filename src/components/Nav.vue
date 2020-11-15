a40bbc935568461aeec2a62d1b34f3d1c9d37122
lnn520 <335341543@qq.com>
2020-11-15 09:48:41 +0800

    添加一个文件引入

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,35 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记账</router-link>|
-    <router-link to="/labels">标签</router-link>|
-    <router-link to="/statistics">统计</router-link>|
+    <router-link to="/money">
+      <svg>
+        <use xlink:href="#label"/>
+      </svg>
+      记账
+    </router-link>
+    |
+    <router-link to="/labels">标签</router-link>
+    |
+    <router-link to="/statistics">统计</router-link>
+    |
   </div>
 </template>
 
-<script>
-export default {
-name: "Nav"
+<script lang="ts">
+//统一引入一个文件
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach((requireContext));
+try{
+  importAll(require.context('../assets/icons',true,/\.svg$/));
+}catch (error) {
+  console.log(error)
 }
+/********/
+export default {
+  name: 'Nav'
+};
 </script>
 
 <style lang="scss" scoped>
-.nav{
+.nav {
   border: 1px solid red;
 }
 </style>
