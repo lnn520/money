6ae9ca5dff7c1ec8130aaaa230ac910ef17fe787
lnn520 <335341543@qq.com>
2020-11-24 17:05:32 +0800

    点击标签可跳转到编辑页面

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,10 +1,10 @@
 <template>
   <layout>
-    <ol class="tags">
-      <li v-for="tag in tags" :key="tag.id"><span>{{ tag.name }}</span>
+    <div class="tags">
+      <router-link v-for="tag in tags" class="tag" :key="tag.id" :to="`labels/edit/${tag.id}`"><span>{{ tag.name }}</span>
         <Icon name="right"/>
-      </li>
-    </ol>
+      </router-link>
+    </div>
     <div class="createTag-wrapper">
       <button class="createTag" @click="createTag">新建标签</button>
     </div>
@@ -40,7 +40,7 @@ tags = tagListModel.data;
   font-size: 16px;
   padding-left: 15px;
 
-  > li {
+  > .tag {
     min-height: 44px;
     display: flex;
     align-items: center;
