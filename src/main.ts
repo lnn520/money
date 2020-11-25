13c47e3c30a5185b83d851ba657095de27780ecb
lnn520 <335341543@qq.com>
2020-11-25 15:20:20 +0800

    封装window.createTag

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,21 @@ import store from './store';
 import Nav from '@/components/Nav.vue';
 import Layout from '@/components/Layout.vue'
 import Icon from '@/components/icon.vue'
+import tagListModel from '@/models/tagListModel';
 
 Vue.config.productionTip = false;
 Vue.component('Nav', Nav);
 Vue.component('Layout', Layout);
 Vue.component('Icon',Icon)
+window.tagList = tagListModel.fetch();
+window.createTag = (name: string)=>{
+    const message =  tagListModel.create(name);
+    if(message === 'duplicated'){
+        window.alert('标签重复')
+    }else {
+        window.alert('添加成功')
+    }
+}
 new Vue({
     router,
     store,
