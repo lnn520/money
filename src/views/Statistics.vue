8335ba6880d0e89fb7756729be714b6d6e162054
lnn520 <335341543@qq.com>
2020-11-28 16:46:30 +0800

    更新css

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,15 +1,45 @@
 <template>
-<layout>
-  statistics
-</layout>
+
+  <Layout>
+    <Tabs class-prefix="type" :data-source="recordTypeList " :value.sync="type"/>
+    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
+  </Layout>
+
+
 </template>
 
 <script lang="ts">
-export default {
-name: "Statistics",
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+import Tabs from '@/components/Tabs.vue';
+import intervalList from '@/constants/intervalList';
+import recordTypeList from '@/constants/recordTypeList';
+
+@Component({
+  components: {Tabs},
+})
+export default class Statistics extends Vue {
+  type = '-';
+  interval = 'day'
+ intervalList = intervalList
+  recordTypeList =  recordTypeList
 }
+
 </script>
 
-<style lang="scss" scoped>
+<style scoped lang="scss">
+::v-deep .type-tabs-item {
+  background: white;
 
+  &.selected {
+    background: #c4c4c4;
+
+    &::after {
+      display: none;
+    }
+  }
+
+}::v-deep li.interval-tabs-item{
+   height: 48px;
+ }
 </style>
